f5b95abb48e98e2008fc80b2d39f2ed5b7c75262
cda1 <jkisl1879@gmail.com>
2026-08-06 20:33:27 +0700

    .

--- Project6/src/Main.cpp
+++ Project6/src/Main.cpp
@@ -1,59 +1,118 @@
 #include <iostream>
-#include <Windows.h>
-#include <thread>
+#include <windows.h>
 using namespace std;
 
-//APP
 bool isRunning = true;
-bool debugMenu = false;
 int tick = 0;
+bool debugMenu = false;
 
-//Player Info
-int health = 100;
+bool lifeStatus = true;
 
+int MaxHealth = 100;
+int MinHealth = 0;
+int MaxThirst = 100;
+int MinThirst = 0;
+int MaxHunger = 100;
+int MinHunger = 0;
 
-void input() 
-{
-	//exit
-	if (GetAsyncKeyState(VK_ESCAPE) & 1) 
-	{
-		isRunning = false;
-	}
-	//debug menu
-	if (GetAsyncKeyState('P') & 1)
-	{
-		debugMenu = !debugMenu;
-	}
-	
-}
-
-
-void gameThread()
-{
-	while(isRunning)
-	{
-		input();
-		tick++;
-		system("cls");
-		if (!debugMenu) {
-			cout << "Health: " << health << endl;
-
-		}
-		if (debugMenu) {
-			cout << "Tick: " << tick << endl;
-		}
-		Sleep(16);
-	}
-}
+int Health = 100;
+int Thirst = 0;
+int Hunger = 100;
 
 int main()
 {
-	thread t(gameThread);
-	Sleep(3000);
+	SetConsoleTitleA("Project6 & Game");
+	while (isRunning)
+	{
+		tick++;
 
-	if (t.joinable()) {
-		t.join();
+		if (Health == 0)
+		{
+			lifeStatus = false;
+		}
+
+		if (Health > MaxHealth)
+		{
+			Health = 100;
+		}
+		if (Health < MinHealth)
+		{
+			Health = 0;
+		}
+
+		if (Thirst > MaxThirst)
+		{
+			Thirst = 100;
+		}
+		if (Thirst < MinThirst)
+		{
+			Thirst = 0;
+		}
+
+		if (Hunger > MaxHunger) 
+		{
+			Hunger = 100;
+		}
+
+		if(Hunger < MinHunger)
+		{
+			Hunger = 0;
+		}
+
+		if (tick % 50 == 0)
+		{
+			Thirst += 2;
+			Hunger += 1;
+		}
+
+		if (Thirst == 100)
+		{
+			if (tick % 50 == 0)
+			{
+				Health -= 1;
+			}
+		}
+
+		if (Hunger == 100)
+		{
+			if (tick % 50 == 0)
+			{
+				Health -= 1;
+			}
+		}
+
+		if (GetAsyncKeyState('P') & 1)
+		{
+			debugMenu = !debugMenu;
+		}
+
+		if (GetAsyncKeyState('V') & 1)
+		{
+			Thirst -= 1;
+		}
+
+		system("cls");
+
+		if (debugMenu)
+		{
+			cout << "tick: " << tick << endl;
+		}
+		else if (lifeStatus == false)
+		{
+			Thirst = 0;
+			cout << "You have died" << endl;
+		}		
+		else 
+		{
+			cout << "Health: " << Health << endl;
+			cout << "Thirst: " << Thirst << endl;
+			cout << "Hunger: " << Hunger << endl;
+			cout << "Debug menu on the p button" << endl;
+			cout << "remove thirst by pressing the v button" << endl;
+		}
+
+		Sleep(16);
 	}
+	system("cls");
 	return 0;
-
 }
